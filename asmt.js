87649557ee38f0cc02f71022e77cce5379460784
EMMANUEL OWOBOWALE <bmtechspace@gmail.com>
2024-08-19 23:45:03 +0100

    initialize commit

---
+++ asmt.js
@@ -0,0 +1,27 @@
+const name = 'Emmanuel'
+const age = 28
+const height = '5.9'
+
+let Bio = 'I am a Fullstack web developer, based in Lagos, Nigeria'
+let Address = '3, muritala esunja street, lagos Nigeria'
+
+const FirstName = 'Emmanuel'
+const LastName = 'Owobowale'
+
+let FullName = FirstName + ' ' + LastName
+
+const message = `My name is ${FirstName}, I am ${age} years old, and I am ${height} feets tall.`
+
+console.log(FullName);
+
+console.log(FullName.length);
+console.log(FullName.toLowerCase());
+console.log(FullName.toUpperCase());
+
+
+console.log(message);
+console.log(Math.round(4.8));
+console.log(Math.floor(4.8));
+console.log(Math.ceil(4.3));
+console.log(Math.max(4, 6, 8, 10, 12, 1, 3, 5, 7 ,9));
+console.log(Math.min(4, 6, 8, 10, 12, 1, 3, 5, 7 ,9));
